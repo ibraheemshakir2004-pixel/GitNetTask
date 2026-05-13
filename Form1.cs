ffa09bc89bb4da3597e772b24aff20a154ad78a2
ibraheemshakir2004-pixel <ibraheemshakir2004@gmail.com>
2026-05-13 17:31:13 +0530

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
+namespace GitNetUzdevums
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDraw_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(this.BackColor);
+
+            Pen pen = new Pen(Color.FromArgb(33, 97, 140), 3);
+            SolidBrush brush = new SolidBrush(Color.FromArgb(120, 52, 152, 219));
+
+            g.FillEllipse(brush, 150, 150, 200, 200);
+            g.DrawEllipse(pen, 150, 150, 200, 200);
+
+            Font font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            SolidBrush textBrush = new SolidBrush(Color.White);
+            g.DrawString("Ibraheem", font, textBrush, 185, 235);
+
+            pen.Dispose();
+            brush.Dispose();
+            font.Dispose();
+            textBrush.Dispose();
+            g.Dispose();
+        }
+    }
+}
