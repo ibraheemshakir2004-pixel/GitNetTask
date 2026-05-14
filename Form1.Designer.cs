a39efc29ec8d5e4de22e3ae90043942a357ae71e
Anaf <ibraheemshakir2004@gmail.com>
2026-05-14 07:16:11 +0530

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,50 +3,78 @@ namespace GitNetUzdevums
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
-
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
-
         private void InitializeComponent()
         {
-            System.Windows.Forms.GroupBox groupBox1 = new System.Windows.Forms.GroupBox();
-            System.Windows.Forms.Button btnDraw = new System.Windows.Forms.Button();
-
-            this.SuspendLayout();
-
-            // GroupBox
-            groupBox1.Text = "Ibraheem Shakir";
-            groupBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            groupBox1.ForeColor = System.Drawing.Color.FromArgb(33, 97, 140);
-            groupBox1.Location = new System.Drawing.Point(20, 20);
-            groupBox1.Size = new System.Drawing.Size(200, 80);
-
-            // Button
-            btnDraw.Text = "DrawCircle";
-            btnDraw.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            btnDraw.BackColor = System.Drawing.Color.FromArgb(33, 97, 140);
-            btnDraw.ForeColor = System.Drawing.Color.White;
-            btnDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            btnDraw.FlatAppearance.BorderSize = 0;
-            btnDraw.Location = new System.Drawing.Point(10, 30);
-            btnDraw.Size = new System.Drawing.Size(120, 30);
-            btnDraw.Click += new System.EventHandler(this.BtnDraw_Click);
-
-            groupBox1.Controls.Add(btnDraw);
-
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnGraph = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout(); 
+            this.groupBox1.Controls.Add(this.btnDraw);
+            this.groupBox1.Controls.Add(this.btnGraph);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(97)))), ((int)(((byte)(140)))));
+            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 80);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Anaf Alilqbal"; 
+            this.btnDraw.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(97)))), ((int)(((byte)(140)))));
+            this.btnDraw.FlatAppearance.BorderSize = 0;
+            this.btnDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDraw.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnDraw.ForeColor = System.Drawing.Color.White;
+            this.btnDraw.Location = new System.Drawing.Point(10, 30);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(120, 30);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "DrawCircle";
+            this.btnDraw.UseVisualStyleBackColor = false;
+            this.btnDraw.Click += new System.EventHandler(this.BtnDraw_Click); 
+            this.btnGraph.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.btnGraph.FlatAppearance.BorderSize = 0;
+            this.btnGraph.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGraph.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnGraph.ForeColor = System.Drawing.Color.White;
+            this.btnGraph.Location = new System.Drawing.Point(145, 30);
+            this.btnGraph.Name = "btnGraph";
+            this.btnGraph.Size = new System.Drawing.Size(120, 30);
+            this.btnGraph.TabIndex = 1;
+            this.btnGraph.Text = "y = cos(x)";
+            this.btnGraph.UseVisualStyleBackColor = false;
+            this.btnGraph.Click += new System.EventHandler(this.BtnGraph_Click);
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(630, 380);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(236, 240, 241);
-            this.ClientSize = new System.Drawing.Size(500, 420);
-            this.Controls.Add(groupBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(673, 520);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "GitNetTask - Ibraheem Shakir";
+            this.Text = "GitNetTask - Anaf Alilqbal";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
         }
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnGraph;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
